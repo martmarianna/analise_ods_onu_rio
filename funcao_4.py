663efd09ff0a2f2ccc2ef72ae97dd9dbf99279cb
Marianna Martins de Oliveira <mariannamartinsdeoliveira@Mariannas-MacBook-Pro.local>
2025-08-04 12:43:33 -0300

    Remove o arquivo 4

--- funcao_4.py
+++
@@ -1,9 +0,0 @@
-def relatorios(d):
-    return print (f'----RELATÕRIO DE ESTOQUE ----\n{d}\n ----FIM DO RELATÕRIO----')
-
-#início do algoritmo
-
-estoque_produtos = {'Macarrão': [8.99, 10.20, 10]}
-total_estoque = 0
-
-relatorios(estoque_produtos)
